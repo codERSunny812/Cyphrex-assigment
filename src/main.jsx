1faf8275dcd3149eac388717620450432c8df4fd
codersunny812 <sengersunny448@gmail.com>
2023-12-23 02:16:40 +0530

    main rendering component

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import ContextProvider from './Context/ApiContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
+    <ContextProvider>
     <ChakraProvider>
     <App />
     </ChakraProvider>
-  </React.StrictMode>,
+    </ContextProvider>
+  </>
 )
