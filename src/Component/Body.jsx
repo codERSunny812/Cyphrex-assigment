ce0381912be986075e1e17eae6b107626f715406
codersunny812 <sengersunny448@gmail.com>
2023-12-23 23:29:36 +0530

    body component

--- src/Component/Body.jsx
+++ src/Component/Body.jsx
@@ -1,29 +1,36 @@
 /* eslint-disable react/prop-types */
 import { Box, Flex } from "@chakra-ui/layout";
+import { useBreakpointValue } from "@chakra-ui/media-query";
 import { TbLineDashed, TbUrgent } from "react-icons/tb";
 import { IoIosAdd } from "react-icons/io";
 import { BsThreeDots } from "react-icons/bs";
-import { apiContext } from '../Context/ApiContext';
+import { apiContext } from "../Context/ApiContext";
 import { useContext } from "react";
-import { PiCellSignalLowBold, PiCellSignalMediumBold, PiCellSignalFullBold } from "react-icons/pi";
+import {
+  PiCellSignalLowBold,
+  PiCellSignalMediumBold,
+  PiCellSignalFullBold,
+} from "react-icons/pi";
 import { FaUserGraduate } from "react-icons/fa";
 import { FcClock } from "react-icons/fc";
 import { FaCircle } from "react-icons/fa6";
 import Lottie from "lottie-react";
-import anim from './anim.json'
+import anim from "./anim.json";
+import useOnline from "../utils/useOnline";
+import offlineAnim from "./noConnection.json";
 
 // cards section
 const Card = (props) => {
-  console.log(props);
+  // console.log(props);
   return (
     <Box
-      my="5%" // Adjusted margin for responsiveness
-      w="90%" // Adjusted width for responsiveness
+      my="5%" //Adjusted margin for responsiveness
+      w="90%" //Adjusted width for responsiveness
       p="13.8px 19.2px"
       borderRadius="12px"
-      bgColor={props.theme === 'light' ? '#ffffff ' : '#161B22'}
-      color={props.theme === 'light' ? 'black' : 'white'} 
-      border={props.theme === 'light' ? 'none' : '1.5px solid gray'} 
+      bgColor={props.theme === "light" ? "#ffffff " : "#161B22"}
+      color={props.theme === "light" ? "black" : "white"}
+      border={props.theme === "light" ? "none" : "1.5px solid gray"}
     >
       <Flex
         h="fit-content"
@@ -51,120 +58,211 @@ const Card = (props) => {
         p="1px 5px"
         w="fit-content"
         h="19.5px"
-        borderRadius='3px'
+        borderRadius="3px"
       >
-        <FaCircle style={{ padding: '1.5px' }} color="#A9A9A9"  />
-        <h2 className="text-[13.6px] px-3 text-gray-500 font-semibold">{props.items.tag[0]}</h2>
+        <FaCircle style={{ padding: "1.5px" }} color="#A9A9A9" />
+        <h2 className="text-[13.6px] px-3 text-gray-500 font-semibold">
+          {props.items.tag[0]}
+        </h2>
       </Flex>
     </Box>
   );
 };
 
-const Body = ({ theme }) => {
-  const data = useContext(apiContext);
+const Body = ({ theme, grouping, ordering }) => {
 
-  return data == null ? (<Lottie animationData={anim} height='80px' width='80px'/>) :  (
+  const isMobile = useBreakpointValue({ base: true, md: false });
+
+  // context api for api fetching
+  const data = useContext(apiContext);
+  console.log(data);
+
+  // hook to check internet connectivity
+
+  const isOnline = useOnline();
+
+  if (!isOnline) {
+    return (
+      <>
+        <div className=" h-96 w-72 m-auto flex justify-center items-center">
+          <Lottie animationData={offlineAnim} className="" />
+        </div>
+      </>
+    );
+  }
+
+  return data == null ? (
+    <div className="flex items-center justify-center">
+      <Lottie
+        animationData={anim}
+        style={{ height: "600px", width: "500px" }}
+      />
+    </div>
+  ) : (
     <>
       <Flex
-        direction={{ base: 'column', md: 'row' }}
+        direction={{ base: "column", md: "row" }}
         wrap="wrap"
         p={4}
         gap="6px"
-        minHeight="100vh" // Ensure the Flex container takes up the full height of the viewport
-        backgroundColor={theme === 'light' ? '#F4F5F8' : '#000000'}
-        color={theme === 'light' ? 'black' : 'white'}
+        minHeight="100vh"
+        backgroundColor={theme === "light" ? "#F4F5F8" : "#000000"}
+        color={theme === "light" ? "black" : "white"}
       >
         {/* box -1 */}
-        <Box  w={{ base: '100%', md: '48%', lg: '270px' }}>
-          <Flex  justifyContent='space-between' w='80%' alignItems="center" >
-            <Flex alignItems="center" >
+        <Box
+          w={{ base: "100%", md: "48%", lg: "270px" }}
+          display={isMobile ? "flex" : "block"} //
+          flexDirection={isMobile ? "column" : "row"}
+          alignItems={isMobile ? "center" : "flex-start"}
+          justifyContent={isMobile ? "center" : "flex-start"}
+          border='2px solid black'
+        >
+          
+
+          <Flex justifyContent="space-between" w="80%" alignItems="center">
+            <Flex alignItems="center">
               <TbLineDashed />
-              <h1 className="capitalize font-bold px-1" >no priority</h1>
+              <h1 className="capitalize font-bold px-1">no priority</h1>
             </Flex>
-            <Flex alignItems="center" px="2px" >
+            <Flex alignItems="center" px="2px">
               <IoIosAdd />
               <BsThreeDots />
             </Flex>
           </Flex>
-          {data?.tickets?.map((items) => {
+          {data?.apiData?.tickets?.map((items) => {
             return items.priority === 0 ? (
-              <Card items={items} key={items.id} theme={theme} />
+              <Card
+                items={items}
+                key={items.id}
+                theme={theme}
+                ordering={ordering}
+                grouping={grouping}
+              />
             ) : null;
           })}
         </Box>
 
         {/* box -2 */}
-        <Box w={{ base: '100%', md: '48%', lg: '270px' }}>
-          <Flex justifyContent='space-between' alignItems="center" w='80%' >
-            <Flex alignItems="center" >
+        <Box
+          w={{ base: "100%", md: "48%", lg: "270px" }}
+          display={isMobile ? "flex" : "block"} //
+          flexDirection={isMobile ? "column" : "row"}
+          alignItems={isMobile ? "center" : "flex-start"}
+          justifyContent={isMobile ? "center" : "flex-start"}
+        >
+          <Flex justifyContent="space-between" alignItems="center" w="80%">
+            <Flex alignItems="center">
               <PiCellSignalLowBold />
-              <h1 className="capitalize font-bold px-1" >low</h1>
+              <h1 className="capitalize font-bold px-1">low</h1>
             </Flex>
-            <Flex alignItems="center" px="2px" >
+            <Flex alignItems="center" px="2px">
               <IoIosAdd />
               <BsThreeDots />
             </Flex>
           </Flex>
-          {data?.tickets?.map((items) => {
+          {data?.apiData?.tickets?.map((items) => {
             return items.priority === 1 ? (
-              <Card items={items} key={items.id} theme={theme} />
+              <Card
+                items={items}
+                key={items.id}
+                theme={theme}
+                ordering={ordering}
+                grouping={grouping}
+              />
             ) : null;
           })}
         </Box>
 
         {/* box - 3 */}
-        <Box w={{ base: '100%', md: '48%', lg: '270px' }}>
-          <Flex justifyContent='space-between' alignItems="center" w='80%' >
-            <Flex alignItems="center" >
+        <Box
+          w={{ base: "100%", md: "48%", lg: "270px" }}
+          display={isMobile ? "flex" : "block"} //
+          flexDirection={isMobile ? "column" : "row"}
+          alignItems={isMobile ? "center" : "flex-start"}
+          justifyContent={isMobile ? "center" : "flex-start"}
+        >
+          <Flex justifyContent="space-between" alignItems="center" w="80%">
+            <Flex alignItems="center">
               <PiCellSignalMediumBold />
-              <h1 className="capitalize font-bold px-1" >medium</h1>
+              <h1 className="capitalize font-bold px-1">medium</h1>
             </Flex>
-            <Flex alignItems="center" px="2px" >
+            <Flex alignItems="center" px="2px">
               <IoIosAdd />
               <BsThreeDots />
             </Flex>
           </Flex>
-          {data?.tickets?.map((items) => {
+          {data?.apiData?.tickets?.map((items) => {
             return items.priority === 2 ? (
-              <Card items={items} key={items.id} theme={theme} />
+              <Card
+                items={items}
+                key={items.id}
+                theme={theme}
+                ordering={ordering}
+                grouping={grouping}
+              />
             ) : null;
           })}
         </Box>
 
         {/* box - 4 */}
-        <Box w={{ base: '100%', md: '48%', lg: '270px' }}>
-          <Flex justifyContent='space-between' alignItems="center" w='80%' >
-            <Flex alignItems="center" >
+        <Box
+          w={{ base: "100%", md: "48%", lg: "270px" }}
+          display={isMobile ? "flex" : "block"} //
+          flexDirection={isMobile ? "column" : "row"}
+          alignItems={isMobile ? "center" : "flex-start"}
+          justifyContent={isMobile ? "center" : "flex-start"}
+        >
+          <Flex justifyContent="space-between" alignItems="center" w="80%">
+            <Flex alignItems="center">
               <PiCellSignalFullBold />
-              <h1 className="capitalize font-bold px-1" >high</h1>
+              <h1 className="capitalize font-bold px-1">high</h1>
             </Flex>
-            <Flex alignItems="center" px="2px" >
+            <Flex alignItems="center" px="2px">
               <IoIosAdd />
               <BsThreeDots />
             </Flex>
           </Flex>
-          {data?.tickets?.map((items) => {
+          {data?.apiData?.tickets?.map((items) => {
             return items.priority === 3 ? (
-              <Card items={items} key={items.id} theme={theme} />
+              <Card
+                items={items}
+                key={items.id}
+                theme={theme}
+                ordering={ordering}
+                grouping={grouping}
+              />
             ) : null;
           })}
         </Box>
 
         {/* box - 5 */}
-        <Box w={{ base: '100%', md: '48%', lg: '270px' }}>
-          <Flex justifyContent='space-between' alignItems="center" w='80%' >
-            <Flex alignItems="center" >
+        <Box
+          w={{ base: "100%", md: "48%", lg: "270px" }}
+          display={isMobile ? "flex" : "block"} //
+          flexDirection={isMobile ? "column" : "row"}
+          alignItems={isMobile ? "center" : "flex-start"}
+          justifyContent={isMobile ? "center" : "flex-start"}
+        >
+          <Flex justifyContent="space-between" alignItems="center" w="80%">
+            <Flex alignItems="center">
               <TbUrgent />
-              <h1 className="capitalize font-bold px-1" >urgent</h1>
+              <h1 className="capitalize font-bold px-1">urgent</h1>
             </Flex>
-            <Flex alignItems="center" px="2px" >
+            <Flex alignItems="center" px="2px">
               <IoIosAdd />
               <BsThreeDots />
             </Flex>
           </Flex>
-          {data?.tickets?.map((items) => {
+          {data?.apiData?.tickets?.map((items) => {
             return items.priority === 4 ? (
-              <Card items={items} key={items.id} theme={theme} />
+              <Card
+                items={items}
+                key={items.id}
+                theme={theme}
+                ordering={ordering}
+                grouping={grouping}
+              />
             ) : null;
           })}
         </Box>
