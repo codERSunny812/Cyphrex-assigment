c9944eb4906dd7306d4382f9cfe2a09db1bbad97
codersunny812 <sengersunny448@gmail.com>
2023-12-24 14:10:13 +0530

    user context updated

--- src/Context/ApiContext.jsx
+++ src/Context/ApiContext.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useEffect , createContext, useState } from "react";
+import { useEffect, createContext, useState } from "react";
 // import { URL } from "../../constant";
 
 
@@ -7,33 +7,33 @@ import { useEffect , createContext, useState } from "react";
 export const apiContext = createContext(null);
 
 const ContextProvider = (props) => {
-// state to store the api response
-const [apiData,setApiData] = useState(null);
- 
-
-useEffect(()=>{
-    // Function to fetch data
-    const getApiData = async () => {
-        try {
-            const response = await fetch("https://tfyincvdrafxe7ut2ziwuhe5cm0xvsdu.lambda-url.ap-south-1.on.aws/ticketAndUsers");
-            const data = await response?.json();
-            setApiData(data);
-        } catch (error) {
-            console.error("Error fetching data:", error);
-        }
-    };
-
-    // Call the fetch function
-    getApiData();
-},[]);
+    // state to store the api response
+    const [apiData, setApiData] = useState(null);
 
 
-  
+    useEffect(() => {
+        // Function to fetch data
+        const getApiData = async () => {
+            try {
+                const response = await fetch("https://tfyincvdrafxe7ut2ziwuhe5cm0xvsdu.lambda-url.ap-south-1.on.aws/ticketAndUsers");
+                const data = await response?.json();
+                setApiData(data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
 
-    return(
-    <apiContext.Provider value={{apiData}} >
-        {props.children}
-    </apiContext.Provider>
+        // Call the fetch function
+        getApiData();
+    }, []);
+
+
+
+
+    return (
+        <apiContext.Provider value={{ apiData }} >
+            {props.children}
+        </apiContext.Provider>
     )
 }
 
